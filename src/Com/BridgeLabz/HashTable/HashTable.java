93e05c3af44c6f5622f121354dc299fc290e40b6
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-15 23:24:52 +0530

    REFACTOR: Ability to find the frequency of word in large paragraph

--- src/Com/BridgeLabz/HashTable/HashTable.java
+++ src/Com/BridgeLabz/HashTable/HashTable.java
@@ -4,7 +4,9 @@ public class HashTable {
     public static void main(String[] args) {
 
             HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation<String, Integer>();
-            String message = "To be or not to be";
+            String message = "Paranoids are not\r\n" + "paranoid because they are paranoid but\r\n"
+                    + "because they keep putting themselves\r\n" + "deliberately into paranoid avoidable\r\n"
+                    + "situations";
 
             /*
              * Split the words when a white space comes up and Convert all the words in
--- src/Com/BridgeLabz/HashTable/Node.java
+++ src/Com/BridgeLabz/HashTable/Node.java
@@ -83,4 +83,3 @@ public class Node<K, V> {
     }
 }
 
-
